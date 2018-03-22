0aea378167d7972596b733c6355b5bac6a46f704
Xiaoquan Kong <u1mail2me@gmail.com>
2018-03-23 00:05:30 +0800

    Better table display for training status info

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import tqdm
 from thinc.neural._classes.model import Model
 from timeit import default_timer as timer
+from collections import OrderedDict
+import tableprint
 
 from ..attrs import PROB, IS_OOV, CLUSTER, LANG
 from ..gold import GoldCorpus
@@ -14,6 +16,20 @@ from .. import util
 from .. import about
 from .. import displacy
 from ..compat import json_dumps
+
+display_metadata = OrderedDict(
+    ("Itn.", "itn"),
+    ("P.Loss", "dep_loss"),
+    ("N.Loss", "ner_loss"),
+    ("UAS", "uas"),
+    ("NER P.", "ents_p"),
+    ("NER R.", "ents_r"),
+    ("NER F.", "ents_f"),
+    ("Tag %", "tags_acc"),
+    ("Token %", "token_acc"),
+    ("cpu_wps", "cpu_wps"),
+    ("gpu_wps", "gpu_wps")
+)
 
 
 @plac.annotations(
@@ -116,7 +132,9 @@ def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
     optimizer = nlp.begin_training(lambda: corpus.train_tuples, device=use_gpu)
     nlp._optimizer = None
 
-    print("Itn.\tP.Loss\tN.Loss\tUAS\tNER P.\tNER R.\tNER F.\tTag %\tToken %")
+    # print table header
+    print(tableprint.header(display_metadata.keys()))
+
     try:
         for i in range(n_iter):
             train_docs = corpus.train_docs(nlp, noise_level=0.0,
@@ -178,6 +196,10 @@ def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
                 util.set_env_log(True)
             print_progress(i, losses, scorer.scores, cpu_wps=cpu_wps,
                            gpu_wps=gpu_wps)
+
+        # print table bottom
+        print(tableprint.bottom(len(display_metadata)))
+
     finally:
         print("Saving model...")
         with nlp.use_params(optimizer.averages):
@@ -196,7 +218,8 @@ def _render_parses(i, to_render):
 
 
 def print_progress(itn, losses, dev_scores, cpu_wps=0.0, gpu_wps=0.0):
-    scores = {}
+    scores = display_metadata
+    scores["itn"] = itn
     for col in ['dep_loss', 'tag_loss', 'uas', 'tags_acc', 'token_acc',
                 'ents_p', 'ents_r', 'ents_f', 'cpu_wps', 'gpu_wps']:
         scores[col] = 0.0
@@ -206,20 +229,9 @@ def print_progress(itn, losses, dev_scores, cpu_wps=0.0, gpu_wps=0.0):
     scores.update(dev_scores)
     scores['cpu_wps'] = cpu_wps
     scores['gpu_wps'] = gpu_wps or 0.0
-    tpl = '\t'.join((
-        '{:d}',
-        '{dep_loss:.3f}',
-        '{ner_loss:.3f}',
-        '{uas:.3f}',
-        '{ents_p:.3f}',
-        '{ents_r:.3f}',
-        '{ents_f:.3f}',
-        '{tags_acc:.3f}',
-        '{token_acc:.3f}',
-        '{cpu_wps:.1f}',
-        '{gpu_wps:.1f}',
-    ))
-    print(tpl.format(itn, **scores))
+
+    # print table row
+    print(tableprint.row(scores.values()), flush=True)
 
 
 def print_results(scorer):
